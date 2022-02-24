43223ad888424098f9eddf3b307fa00b89d684e4
Syenna Graham <syenna.graham@gmail.com>
2022-02-23 22:14:38 -0500

    Update setup_rp.sh

--- setup_rp.sh
+++ setup_rp.sh
@@ -1,7 +1,7 @@
 apt-get install python-tk screen
 pip3 install pyserial
-echo "alias compile="arduino-cli compile --fqbn arduino:avr:uno "'  >> ~/.bashrc 
-echo 'alias upload=“arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno "'  >> ~/.bashrc 
+echo "alias compile="arduino-cli compile --fqbn arduino:avr:uno ""  >> ~/.bashrc 
+echo 'alias upload="arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno "'  >> ~/.bashrc 
 echo 'export DISPLAY=:1'  >> ~/.bashrc 
 
 # connect BT keyboard and mouse - https://www.cnet.com/tech/computing/how-to-setup-bluetooth-on-a-raspberry-pi-3/ 
